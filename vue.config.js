2c705a552df8e5e171678804f057e318dcc1b905
José Ignacio Escribano <jiep12345@hotmail.com>
2020-12-29 17:23:01 +0100

    Test

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,20 @@ module.exports = {
   pwa: {
     themeColor: "#dc2626",
     msTileColor: "#dc2626",
+    workboxPluginMode: "GenerateSW",
     workboxOptions: {
-      skipWaiting: true,
-      clientsClaim: true,
+      navigateFallback: "/index.html",
+      runtimeCaching: [{
+        urlPattern: new RegExp('.'),
+        handler: 'networkFirst',
+        options: {
+          networkTimeoutSeconds: 20,
+          cacheName: 'cache',
+          cacheableResponse: {
+            statuses: [200],
+          },
+        },
+      }]
     }
   }
 };
