8f34f461f4e8ca24dc736888c4f99538f72d2cd2
José Ignacio Escribano <jiep12345@hotmail.com>
2022-05-14 18:13:18 +0200

    Update Vue to v3
    Closes #115

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template lang="pug">
-  .app.w-screen.h-screen.flex.flex-col.bg-metro.bg-contain.bg-center
-    Nav/
-    Metro.flex-1/
+.app.w-screen.h-screen.flex.flex-col.bg-metro.bg-contain.bg-center
+  Nav/
+  Metro.flex-1/
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import { defineComponent } from 'vue'
+
 import Metro from './components/Metro.vue'
 import Nav from './components/Nav.vue'
 
-export default Vue.extend({
-  name: 'App',
+export default defineComponent({
   components: {
     Metro,
     Nav
