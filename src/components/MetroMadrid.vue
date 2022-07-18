fe3c6b8f0fbcb18fd186baec56543893aa0c7f45
José Ignacio Escribano <jiep@users.noreply.github.com>
2022-07-18 18:14:53 +0000

    Fix error

--- src/components/MetroMadrid.vue
+++ src/components/MetroMadrid.vue
@@ -56,7 +56,7 @@ export default defineComponent({
       stations: new Array<Station>(),
       selectedOrigin: '',
       selectedDestiny: '',
-      shortestPath: new Array<string>,
+      shortestPath: new Array<string>(),
       distance: 0,
       clicked: false,
       sameStations: false
