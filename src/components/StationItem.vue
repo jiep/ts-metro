0b92b90b9a25b78e6c59ad5f0dfd263d4e9eeb62
José Ignacio Escribano <jiep@users.noreply.github.com>
2023-06-23 18:26:23 +0000

    Fix text size

--- src/components/StationItem.vue
+++ src/components/StationItem.vue
@@ -2,7 +2,7 @@
 .flex.flex-col.items-center.w-full
   .flex.bg-red-50.px-4.py-2.border-l-4.border-r-4.border.border-red-600.shadow-xl.rounded-xl.justify-between.w-full(class="md:w-3/4")
     .flex-row.flex.mr-4
-      span.text-left.text-xl.text-gray-800 {{station.name}}
+      span.text-left.antialiased.text-gray-800 {{station.name}}
     .flex.items-center.flex.flex-nowrap.overflow-auto
       template(v-for="line in station.getLines()")
         span.font-medium.text-xs.text-center.rounded.mr-1.py-1.px-2(class="last:mr-0" :style="[{'background-color': lines.get(line).bgColor}, {'color': lines.get(line).textColor}, {'border': `1px solid ${lines.get(line).borderColor}`}]") {{line}}
