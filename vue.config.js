4f1e6178493049083091f1bc76321b316f8f22fc
José Ignacio Escribano <jiep@users.noreply.github.com>
2023-06-23 18:01:52 +0000

    Fix error with icons

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,16 @@
 module.exports = {
   pwa: {
     themeColor: '#2563eb',
-    msTileColor: '#2563eb'
+    msTileColor: '#2563eb',
+    iconPaths: {
+      faviconSVG: null,
+      favicon32: 'img/icons/favicon.ico',
+      favicon16: 'img/icons/favicon.ico',
+      appleTouchIcon: 'img/icons/apple-touch-icon.png',
+      maskIcon: 'img/icons/icon-192-maskable.png',
+      maskIcon512: 'img/icons/icon-512-maskable.png',
+      msTileImage: null
+    }
     // workboxPluginMode: "GenerateSW",
     // workboxOptions: {
     //   navigateFallback: "/index.html",
