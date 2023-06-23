545554c36959244f1511d987ea4fb428b05e0c90
José Ignacio Escribano <jiep@users.noreply.github.com>
2023-06-23 19:52:05 +0000

    Disable text select from navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,7 +4,7 @@ nav.bg-blue-600
     .flex.items-center.justify-center.m-4
       .flex.text-white.items-center
         img.h-12.mr-2(src="/img/icons/MetroMadridLogo.png")
-        .flex-col
+        .flex-col.select-none("md:select-text")
           h1.text-xl Metro de Madrid
           p.text-xs Busca la ruta más corta entre estaciones
 </template>
