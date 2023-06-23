83611402da4a4b57278a144c8886c6d100915bf5
José Ignacio Escribano <jiep@users.noreply.github.com>
2023-06-23 16:31:26 +0000

    Update NavBar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,10 +1,10 @@
 <template lang="pug">
 nav.bg-blue-600
-  .max-w-screen-xl.px-4.mx-auto(class='sm:px-6 lg:px-8')
-    .flex.items-center.justify-center.h-16
+  .max-w-screen-xl.px-4.mx-auto
+    .flex.items-center.justify-center.m-4
       .flex.text-white.items-center
         img.h-12.mr-2(src="/img/icons/MetroMadridLogo.png")
-        .flex.flex-col
+        .flex-col
           h1.text-xl Metro de Madrid
           p.text-xs Busca la ruta más corta entre estaciones
 </template>
