b50131dbec513c6c9fabee7af07e2f30bad078bd
José Ignacio Escribano <jiep@users.noreply.github.com>
2023-06-23 16:08:15 +0000

    Add responsive IU #190

--- src/components/MetroMadrid.vue
+++ src/components/MetroMadrid.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
-.flex.opacity-90.overflow-y-auto
-  section.flex.p-8.justify-center.items-center
-    form.bg-gray-50.shadow-xl.rounded-lg.p-8
+.opacity-90.overflow-y-auto(class="md:flex")
+  section.flex.m-4.justify-center.items-center
+    form.bg-gray-50.shadow-xl.rounded-lg.p-8.w-full
       label.block.mb-4
         span.text-gray-700 Estación de origen
         select.block.w-full.mt-1.border.border-gray-600.rounded-md(v-model='selectedOrigin', class='focus:border-gray-500 focus:bg-white focus:ring-0')
@@ -16,14 +16,14 @@
 
       .flex.justify-items-end
         button.flex-1.border.border-blue-500.bg-blue-500.text-white.rounded-md.px-4.py-2.transition.duration-500.ease.select-none(@click='onClick', :disabled="selectedOrigin === '' && selectedDestiny === ''", type='button', class='hover:bg-blue-600 focus:outline-none focus:shadow-outline disabled:cursor-not-allowed disabled:bg-blue-500') Buscar ruta más corta
-  section.flex-1.p-8.overflow-y-auto
-    .bg-gray-50.shadow-xl.rounded-lg.p-8.h-full
+  section.flex.flex-1.m-4.overflow-y-auto
+    .bg-gray-50.shadow-xl.rounded-lg.p-8.h-full.w-full
       template(v-if="clicked")
         .flex.flex-col.overflow-y-auto.h-full
           StatsItem.bg-gray-50(:from="metro.getStationById(selectedOrigin).getName()",
                     :to="metro.getStationById(selectedDestiny).getName()",
                     :distance="distance")
-          section.flex.flex-col.overflow-y-auto.h-full.border-t.border-gray-300.py-8
+          section.flex.flex-col.overflow-y-auto.h-full.border-t.border-gray-300.pt-8
             StationList(:stations="shortestPath", :lines="linesConfig")
       section.flex.flex-col.justify-center.items-center.h-full.text-center(v-else)
         WarningItem(v-show="!sameStations" message="Selecciona estaciones de origen y destino para calcular la ruta mínima")
