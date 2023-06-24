d2fad1057f5f375608aa9dda2d1443381a7bf0c6
José Ignacio Escribano <jiep@users.noreply.github.com>
2023-06-24 13:48:27 +0000

    Refactor AlertItem

--- src/components/AlertItem.vue
+++ src/components/AlertItem.vue
@@ -2,8 +2,7 @@
 .p-4.text-red-700.border.bg-red-50.w-full(class='border-red-900/10' role='alert')
   .flex.flex-col.place-items-center
     span.p-2.text-white.bg-red-600.rounded-full
-      svg.w-12.h-12(xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor' stroke-width='2')
-        path(stroke-linecap='round' stroke-linejoin='round' d='M20.618 5.984A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016zM12 9v2m0 4h.01')
+      AlertIcon.w-12.h-12
     p.mt-4
       strong.block.opacity-90
         | {{message}}
@@ -12,10 +11,15 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 
+import AlertIcon from '@/components/AlertIcon.vue';
+
 export default defineComponent({
   name: 'AlertItem',
   props: [
     'message'
-  ]
+  ],
+  components: {
+    AlertIcon
+  }
 })
 </script>
