13eb1398b18c3795e29314a4f495356c62ed0004
José Ignacio Escribano <jiep@users.noreply.github.com>
2023-06-24 06:23:11 +0000

    Move logo

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,7 +3,7 @@ nav.bg-blue-600
   .max-w-screen-xl.px-4.mx-auto
     .flex.items-center.justify-center.m-4
       .flex.text-white.items-center
-        img.h-12.mr-2(src="/img/icons/MetroMadridLogo.png")
+        img.h-12.mr-2(src="/img/MetroMadridLogo.png")
         .flex-col.select-none("md:select-text")
           h1.text-xl Metro de Madrid
           p.text-xs Busca la ruta más corta entre estaciones
