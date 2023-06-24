9d1161f47747f8520019bb9dc0bf65e8a7f7da65
José Ignacio Escribano <jiep@users.noreply.github.com>
2023-06-24 13:52:03 +0000

    Fix error

--- src/components/AlertItem.vue
+++ src/components/AlertItem.vue
@@ -11,7 +11,7 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 
-import AlertIcon from '@/components/AlertIcon.vue';
+import AlertIcon from '@/components/AlertIcon.vue'
 
 export default defineComponent({
   name: 'AlertItem',
