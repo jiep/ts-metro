8b826d0c54ea958ed809e5957a17c722c22182bf
José Ignacio Escribano <jiep@users.noreply.github.com>
2023-06-25 08:03:05 +0200

    Update NavBar.vue

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,7 +3,7 @@ nav.bg-blue-600
   .max-w-screen-xl.px-4.mx-auto
     .flex.items-center.justify-center.m-4
       .flex.text-white.items-center
-        LogoIcon.h-12.mr-2(src="/img/MetroMadridLogo.png")
+        LogoIcon.h-12.mr-2
         .flex-col.select-none("md:select-text")
           h1.text-xl Metro de Madrid
           p.text-xs Busca la ruta más corta entre estaciones
