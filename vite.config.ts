14cc70eaeba4efa932334aa2e2cc1a890a8ca6b3
José Ignacio Escribano <jiep@users.noreply.github.com>
2024-12-27 15:49:20 +0000

    Update config

--- vite.config.ts
+++ vite.config.ts
@@ -40,9 +40,6 @@ export default defineConfig({
           },
         ],
       },
-      includeAssets: [
-        "**/*",
-      ],
       manifest: {
         name: 'ts-metro',
         short_name: 'ts-metro',
