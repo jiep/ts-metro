16d0598373846ac370b780620e5b5d1b8153f6fc
José Ignacio Escribano <jiep@users.noreply.github.com>
2024-12-28 15:21:13 +0000

    Fix warning

--- vite.config.ts
+++ vite.config.ts
@@ -73,13 +73,13 @@ export default defineConfig({
             src: 'img/icons/android-chrome-maskable-192x192.png',
             sizes: '192x192',
             type: 'image/png',
-            purpose: ['maskable'],
+            purpose: ['any', 'maskable'],
           },
           {
             src: 'img/icons/android-chrome-maskable-512x512.png',
             sizes: '512x512',
             type: 'image/png',
-            purpose: ['maskable'],
+            purpose: ['any', 'maskable'],
           }
         ],
         ...{ version },
