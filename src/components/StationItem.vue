2ea7799fac64513c00df391f5d98ca1970775a26
José Ignacio Escribano <jiep@users.noreply.github.com>
2025-04-26 07:02:30 +0000

    Add transfers to route

--- src/components/StationItem.vue
+++ src/components/StationItem.vue
@@ -6,7 +6,14 @@
     .flex.items-center.flex.flex-nowrap.overflow-auto
       template(v-for="line in station.getLines()")
         span.font-medium.text-xs.text-center.rounded.mr-1.py-1.px-2(class="last:mr-0" :style="[{'background-color': lines.get(line).bgColor}, {'color': lines.get(line).textColor}, {'border': `1px solid ${lines.get(line).borderColor}`}]") {{line}}
-  SeparatorIcon.rotate-90.w-4.fill-current.text-blue-400.m-2(class="last:display-none", v-if="!isLast")
+  .flex.flex-row.items-center.justify-center
+    SeparatorIcon.rotate-90.w-4.fill-current.text-blue-400.m-2(class="last:display-none", v-if="!isLast") 
+    p(v-if="transfer") 
+      span.mr-2 Transbordo de
+      span.mr-2.font-medium.text-xs.text-center.rounded.mr-1.py-1.px-2(class="last:mr-0" :style="[{'background-color': lines.get(transfer.fromLine).bgColor}, {'color': lines.get(transfer.fromLine).textColor}, {'border': `1px solid ${lines.get(transfer.fromLine).borderColor}`}]") {{transfer.fromLine}}
+      span.mr-2 a
+      span.font-medium.text-xs.text-center.rounded.mr-1.py-1.px-2(class="last:mr-0" :style="[{'background-color': lines.get(transfer.toLine).bgColor}, {'color': lines.get(transfer.toLine).textColor}, {'border': `1px solid ${lines.get(transfer.toLine).borderColor}`}]") {{transfer.toLine}}
+
 </template>
 
 <script lang="ts">
@@ -16,7 +23,7 @@ import SeparatorIcon from '@/components/SeparatorIcon.vue'
 
 export default defineComponent({
   name: 'StationItem',
-  props: ['station', 'lines', 'isLast'],
+  props: ['station', 'lines', 'isLast', 'transfer'],
   components: {
     SeparatorIcon,
   },
