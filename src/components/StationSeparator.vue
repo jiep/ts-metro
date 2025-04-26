85c89bcd20bf92224060cbfe28003fd23e5b6153
José Ignacio Escribano <jiep@users.noreply.github.com>
2025-04-26 13:50:57 +0000

    Add new component for station separator

---
+++ src/components/StationSeparator.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+svg(xmlns='http://www.w3.org/2000/svg' viewBox='0 0 384 512')
+  rect(x="160" y="0" width="64" height="512" :fill="color")
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'StationSeparator',
+  props: {
+    color: {
+      type: String,
+      default: '#000',
+    },
+  },
+})
+</script>
