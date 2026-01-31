c942b208c115a7782c875f60a0f6ac544ca30f8e
José Ignacio Escribano <jiep@users.noreply.github.com>
2026-01-31 15:51:01 +0000

    Fix types in code

--- src/lib/graphs/Graph.ts
+++ src/lib/graphs/Graph.ts
@@ -3,36 +3,45 @@ import GraphClass from './GraphClass'
 export default class Graph {
   private class: GraphClass
   private verticesNumber: number
-  private adjacencyMatrix: number[][] = [[]]
+  private adjacencyMatrix: number[][] = []
 
   constructor(c: GraphClass, verticesNumber: number) {
     this.class = c
     this.verticesNumber = verticesNumber
 
     for (let i = 0; i < this.verticesNumber; i++) {
-      this.adjacencyMatrix[i] = []
+      const row = this.adjacencyMatrix[i] ?? (this.adjacencyMatrix[i] = [])
+      this.adjacencyMatrix[i] ??= []
       for (let j = 0; j < this.verticesNumber; j++) {
-        this.adjacencyMatrix[i][j] = 1
+        row[j] = 1
       }
     }
   }
 
+  protected ensure(i: number, j: number) {
+    this.adjacencyMatrix[i] ??= []
+    this.adjacencyMatrix[j] ??= []
+  }
+
   public connect(i: number, j: number): void {
-    this.adjacencyMatrix[i][j] = 1
+    this.ensure(i, j)
+    this.adjacencyMatrix[i]![j] = 1
     if (this.class === GraphClass.UNDIRECTED) {
-      this.adjacencyMatrix[j][i] = 1
+      this.adjacencyMatrix[j]![i] = 1
     }
   }
 
   public disconnect(i: number, j: number): void {
-    this.adjacencyMatrix[i][j] = 0
+    this.ensure(i, j)
+    this.adjacencyMatrix[i]![j] = 0
     if (this.class === GraphClass.UNDIRECTED) {
-      this.adjacencyMatrix[j][i] = 0
+      this.adjacencyMatrix[j]![i] = 0
     }
   }
 
   public areConnected(i: number, j: number): boolean {
-    return this.adjacencyMatrix[i][j] === 1
+    this.ensure(i, j)
+    return this.adjacencyMatrix[i]![j] === 1
   }
 
   public vertices(): number {
